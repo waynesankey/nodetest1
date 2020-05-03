b55a49899a3da951b45167fe6aa78ea5944f6b41
Wayne Sankey <flyinggingernut@gmail.com>
2020-05-03 21:35:50 +0000

    escaped apostrophe in comment

--- nodeTest.js
+++ nodeTest.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(
 	function(req,res){
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('Hello from Wayne's BeagleBone Black!\n');
+		res.end('Hello from Wayne\'s BeagleBone Black!\n');
 	});
 server.listen(5050);
 console.log('BBB Web Server running at http://192.168.1.195:5050');
